83e604e35844685ef7864d72a4a468f17591ddaa
George Lester <glester491@gmail.com>
2016-08-10 21:37:42 -0700

    Revised doc comments for default Parameters implementation

--- parameters.go
+++ parameters.go
@@ -4,17 +4,21 @@ import (
 	"errors"
 )
 
-// Parameters is a collection of named parameters that are accessible via the
-// Get method.
+/*
+	Parameters is a collection of named parameters that can be used by an EvaluableExpression to retrieve parameters
+	when an expression tries to use them.
+*/
 type Parameters interface {
-	// Get gets the parameter of the given name
+
+	/*
+		Get gets the parameter of the given name, or an error if the parameter is unavailable.
+		Failure to find the given parameter should be indicated by returning an error.
+	*/
 	Get(name string) (interface{}, error)
 }
 
-// MapParameters is an implementation of the Parameters interface using a map.
 type MapParameters map[string]interface{}
 
-// Get implemetns the method from Parameters
 func (p MapParameters) Get(name string) (interface{}, error) {
 
 	value, found := p[name]
