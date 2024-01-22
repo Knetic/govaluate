05f05ab2b1615c890abba56bdcd1520e279f5a93
zhangjianxiongZJX <47131117+zhangjianxiongZJX@users.noreply.github.com>
2024-01-22 20:30:24 +0800

    Update sanitizedParameters.go

--- sanitizedParameters.go
+++ sanitizedParameters.go
@@ -1,5 +1,10 @@
 package govaluate
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 // sanitizedParameters is a wrapper for Parameters that does sanitization as
 // parameters are accessed.
 type sanitizedParameters struct {
@@ -12,31 +17,35 @@ func (p sanitizedParameters) Get(key string) (interface{}, error) {
 		return nil, err
 	}
 
-	return castToFloat64(value), nil
+	return castToNumber(value), nil
 }
 
-func castToFloat64(value interface{}) interface{} {
+func castToNumber(value interface{}) interface{} {
 	switch value.(type) {
 	case uint8:
-		return float64(value.(uint8))
+		return json.Number(strconv.FormatInt(int64(value.(uint8)), 10))
 	case uint16:
-		return float64(value.(uint16))
+		return json.Number(strconv.FormatInt(int64(value.(uint16)), 10))
 	case uint32:
-		return float64(value.(uint32))
+		return json.Number(strconv.FormatInt(int64(value.(uint32)), 10))
 	case uint64:
-		return float64(value.(uint64))
+		return json.Number(strconv.FormatInt(int64(value.(uint64)), 10))
+	case uint:
+		return json.Number(strconv.FormatInt(int64(value.(uint)), 10))
 	case int8:
-		return float64(value.(int8))
+		return json.Number(strconv.FormatInt(int64(value.(int8)), 10))
 	case int16:
-		return float64(value.(int16))
+		return json.Number(strconv.FormatInt(int64(value.(int16)), 10))
 	case int32:
-		return float64(value.(int32))
+		return json.Number(strconv.FormatInt(int64(value.(int32)), 10))
 	case int64:
-		return float64(value.(int64))
+		return json.Number(strconv.FormatInt(value.(int64), 10))
 	case int:
-		return float64(value.(int))
+		return json.Number(strconv.FormatInt(int64(value.(int)), 10))
 	case float32:
-		return float64(value.(float32))
+		return json.Number(strconv.FormatFloat(float64(value.(float32)), 'f', 10, 64))
+	case float64:
+		return json.Number(strconv.FormatFloat(value.(float64), 'f', 10, 64))
 	}
 
 	return value
